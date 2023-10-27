441661f2ba7235081023756f372357d97abacc01
FatouBintouDiallo <fatdiallo128@gmail.com>
2023-10-27 21:26:58 +0200

    DOM

---
+++ script.js
@@ -0,0 +1,52 @@
+
+function checknumber(){
+    let prix = +document.getElementById('pu').innerText.split(' ')[0]
+    console.log(prix)
+    let quantite = +document.getElementById('qte').value
+    console.log(quantite)
+    let total= prix*quantite;
+    document.getElementById('TOTAL').innerText = total + ('fr')
+
+    let price= +document.getElementById('pu2').innerText.split(' ')[0]
+    console.log(prix)
+    let quanti = +document.getElementById('qte2').value
+    console.log(quantite)
+    let total2= price*quanti;
+    document.getElementById('TOTAL2').innerText = total2 + ('fr')
+
+    let pri= +document.getElementById('pu3').innerText.split(' ')[0]
+    console.log(prix)
+    let quantite1 = +document.getElementById('qte3').value
+    console.log(quantite)
+    let total3= pri*quantite1;
+    document.getElementById('TOTAL3').innerText = total3 + ('fr')
+   
+}
+function TOTAL(){
+    let prix = +document.getElementById('pu').innerText.split(' ')[0]
+    console.log(prix)
+    let quantite = +document.getElementById('qte').value
+    console.log(quantite)
+    let total= prix*quantite;
+    document.getElementById('TOTAL').innerText = total + ('fr')
+
+    let price= +document.getElementById('pu2').innerText.split(' ')[0]
+    console.log(prix)
+    let quanti = +document.getElementById('qte2').value
+    console.log(quantite)
+    let total2= price*quanti;
+    document.getElementById('TOTAL2').innerText = total2 + ('fr')
+
+    let pri= +document.getElementById('pu3').innerText.split(' ')[0]
+    console.log(prix)
+    let quantite1 = +document.getElementById('qte3').value
+    console.log(quantite)
+    let total3= pri*quantite1;
+    document.getElementById('TOTAL3').innerText = total3 + ('fr')
+
+    let TOTAL5 = total + total2 + total3
+    console.log(TOTAL5)
+    document.getElementById("xxx").innerText= TOTAL5 + ('fr')
+    
+}
+
